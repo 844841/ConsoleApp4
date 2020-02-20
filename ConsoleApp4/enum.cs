61e78253705975e7c1038e9081bc1690e56ab990
15341a1205 <41323544+15341a1205@users.noreply.github.com>
2020-02-20 14:44:24 +0530

    Initial commit

---
+++ ConsoleApp4/enum.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace subject
+{
+    class Program
+    {
+        public enum department
+        {
+            physics = 10,
+            chemistry,
+            mathematics = 15,
+            biology = 18,
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("department of mathematics:{0}", (int)department.mathematics);
+            Console.ReadKey();
+
+            
+            
+
+        }
+    }
+    
+}
